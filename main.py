1d546fd3530737880b43a0eb0833b37377a72dd2
Bhargavi <bhargavivijaykumarkulkarni@gmail.in>
2026-05-28 10:20:12 +0530

    testing webook

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from fastapi import FastAPI
+# PRSentry - AI Code Review Bot
+from fastapi import FastAPI, Request
 
 app=FastAPI()
 
 @app.get("/")
 def home():
     return {"message":"Bot is alive!"}
+
+@app.post("/webhook")
+async def webhook(request: Request):
+    data=await request.json()
+    print(data)
+    return {"status": "recieved"}
